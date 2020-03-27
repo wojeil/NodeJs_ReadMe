bd04c544212456d4244df78c369c1f39a63fbfd5
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-27 17:15:25 -0400

    files added

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// Require all npm packages and files
+
+const questions = [
+// questions to user using "enquirer"
+];
+
+function init() {
+
+// Logic for this app shoud be here. You can create more functions if you like. This is just a template for you to start your Homework but it is not required for you to use it.
+
+}
+
+init();
