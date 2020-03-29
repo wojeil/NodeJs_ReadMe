d3dadb262794d766b258e36d743514c8ef9ba5c1
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-28 20:33:27 -0400

    adding the prompt

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ var generateMarkdown = require("./utils/generateMarkdown.js");
 const questions = [
 // questions to user using "enquirer"
 ];
-
+inquirer
+  .prompt([
+    
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  })
+  .catch(error => {
+    if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
 function init() {
 
 // Logic for this app shoud be here. You can create more functions if you like. This is just a template for you to start your Homework but it is not required for you to use it.
